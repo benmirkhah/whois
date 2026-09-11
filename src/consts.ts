4cc27643d4ea9aa13e1ed92aebffebd169848d91
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2026-09-10 21:02:01 -0700

    Meta Tags updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
+//Global data go here
 export const SITE_TITLE = 'BEN WHO?';
-export const SITE_DESCRIPTION = 'Ben Mirkhah - Web Performance Extraordinaire';
+export const SITE_DESCRIPTION = 'Ben Mirkhah builds ultra high performance digital experiences.';
